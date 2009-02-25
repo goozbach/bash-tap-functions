e52ad45b66880ea565a0c4d5cf42fc7300f3b8ce
patrickl <patrickl@c3086684-d568-4a45-a57d-89166b08296d>
2009-02-25 00:58:27 +0000

    ok
    git-svn-id: http://svn.solucorp.qc.ca/repos/solucorp/JTap/trunk@416 c3086684-d568-4a45-a57d-89166b08296d

--- tap.hs
+++ tap.hs
@@ -3,6 +3,7 @@ module TAP where
 import System.IO
 import System.Exit
 import Control.Monad.State
+import Control.Exception
 import Text.Regex.Posix
 
 
@@ -58,7 +59,7 @@ planSkipAll s = do
             Just s -> s
             otherwise -> ""
     modify (\x -> x {planSet = True, skipAll = True})
-    lift $ _exit 0
+    _exit $ Just 0
 
 
 planTests :: Int -> Maybe String -> TAP Int
@@ -91,24 +92,24 @@ fail s = do
 
 
 ok :: Bool -> Maybe String -> TAP Bool
-ok r s = do
+ok result msg = do
     _checkNotPlanned
     modify (\x -> x {executedTests = executedTests x + 1})
 
-    case s of
+    case msg of
         Just s -> when (_matches s "^[0-9]+$") $ do
             diag $ "    You named your test '" ++ s ++ "'.  You shouldn't use numbers for your test names."
-            diag "    Very confusing."
+            diag $ "    Very confusing."
         otherwise -> return ()
 
-    when (not r) $ do
+    when (not result) $ do
         lift $ putStr "not "
         modify (\x -> x {failedTests = failedTests x + 1})
 
     ts <- get
     lift . putStr $ "ok " ++ (show $ executedTests ts)
 
-    case s of
+    case msg of
         Just s -> lift . putStr $ " - " ++ s
         otherwise -> return ()
   
@@ -118,7 +119,7 @@ ok r s = do
 
     -- STACK TRACE
 
-    return r
+    return result
 
 
 _matches :: String -> String -> Bool
@@ -127,44 +128,146 @@ _matches _ "" = False
 _matches target pattern = target =~ pattern :: Bool
 
 
+_is_diag :: (Show a) => a -> a -> TAP ()
+_is_diag result expected = do
+    diag $ "         got: '" ++ (show result) ++ "'"
+    diag $ "    expected: '" ++ (show expected) ++ "'"
+
+
+is :: (Show a, Eq a) => a -> a -> Maybe String -> TAP Bool
+is result expected msg = do
+    rc <- ok (result == expected) msg
+    when (not rc) $ do
+        diag $ "           got: '" ++ (show result) ++ "'"
+        diag $ "      expected: '" ++ (show expected) ++ "'"
+    return rc
+
+
+isnt :: (Show a, Eq a) => a -> a -> Maybe String -> TAP Bool
+isnt result expected msg = do
+    rc <- ok (result /= expected) msg
+    when (not rc) $ do
+        diag $ "           got: '" ++ (show result) ++ "'"
+        diag $ " didn't expect: '" ++ (show expected) ++ "'"
+    return rc
+
+
+like :: String -> String -> Maybe String -> TAP Bool
+like target pattern msg = do
+    rc <- ok (_matches target pattern) msg
+    when (not rc) $ do
+       diag $ "    '" ++ target ++ "' doesn't match '" ++ pattern ++ "'"
+    return rc
+
+
+unlike :: String -> String -> Maybe String -> TAP Bool
+unlike target pattern msg = do
+    rc <- ok (not $ _matches target pattern) msg
+    when (not rc) $ do
+       diag $ "    '" ++ target ++ "' matches '" ++ pattern ++ "'"
+    return rc
+
+
+skip :: Int -> Maybe String -> TAP ()
+skip n reason = do
+    let msg = case reason of Just s -> s
+                             otherwise ->  ""
+    forM_ [1 .. n] (\n' -> do
+        modify (\x -> x {executedTests = executedTests x + 1})
+        ts <- get
+        lift . putStrLn $ "ok " ++ (show $ executedTests ts) ++ " # skip: " ++ msg)
+    return ()
+
+
 diag :: String -> TAP ()
 diag s = do 
-  lift . putStrLn $ "# " ++ s
+    lift . putStrLn $ "# " ++ s
 
 
 _die :: String -> TAP a
 _die s = do 
-  lift $ hPutStrLn stderr s
-  modify (\x -> x {testDied = True})
-  lift $ _exit 255
+    lift $ hPutStrLn stderr s
+    modify (\x -> x {testDied = True})
+    _exit $ Just 255
 
 
 bailOut :: String -> TAP a
 bailOut s = do
-  lift $ hPutStrLn stderr s
-  lift $ _exit 255
+    lift $ hPutStrLn stderr s
+    _exit $ Just 255
 
 
-_exit :: Int -> IO a
-_exit 0  = exitWith ExitSuccess
-_exit rc = exitWith $ ExitFailure rc
+_cleanup :: Int -> TAP Int
+_cleanup rc = do
+    let s n = if (n > 1) then "s" else ""
+    ts <- get
+	if (not $ planSet ts)
+        then do
+            diag "Looks like your test died before it could output anything."
+            return rc
+        else if (testDied ts)
+            then do
+                diag $ "Looks like your test died just after " ++ (show $ executedTests ts)
+                return rc
+            else
+                if (( _skip_all == 0 && _no_plan != 0 )) ; then
+		_print_plan $_executed_tests
+	fi
+
+	when (((not $ noPlan ts)&&((_expected_tests) < (_executed_tests)) $ do
+		local extra=$(( _executed_tests - _expected_tests ))
+		diag "Looks like you planned $_expected_tests test$s but ran $extra extra."
+		rc=-1 ;
+	fi
+
+	when ((not $ noPlan ts)&&((expectedTests ts) > (_executedTests ts))) $ do
+		diag $ "Looks like you planned " ++ (show $ expectedTests ts)) ++ " test" ++ (s $ expectedTests ts) 
+            ++ " but only ran " ++ (show $ executedTests ts)
+
+	when (failedTests ts > 0) $ do
+		diag $ "Looks like you failed " ++ (show $ failedTests ts)) ++ " test" ++ (s $ failedTests ts) 
+            ++ " of " ++ (show $ executedTests ts)
+
+	return rc
 
 
-runSuite :: TAP a -> IO (a, TapState)
-runSuite s = runStateT s initState
+_exitStatus :: TAP Int
+_exitStatus = do
+    ts <- get
+    if ((noPlan ts)||(not $ planSet ts)) 
+        then return $ failedTests ts
+        else if ((expectedTests ts) < (executedTests ts))
+            then return $ (executedTests ts) - (expectedTests ts)
+            else return $ ((failedTests ts) + ((expectedTests ts) - (executedTests ts)))
 
-main = runSuite $ do
-   planTests 5 Nothing
-   ok True $ Just "True"
+
+_exit :: Maybe Int -> TAP a
+_exit mrc = do
+    es <- _exitStatus
+    let rc = _cleanup $ case mrc of Just rc -> rc
+                                    otherwise -> es
+    lift . exitWith $ if (rc == 0) 
+                   then ExitSuccess
+                   else ExitFailure rc
+ 
+
+runTests :: TAP a -> IO (a, TapState)
+runTests s = runStateT (s >> _exit Nothing) initState
+
+
+main = runTests $ do
+   planTests 8 Nothing
+   ok True $ Just "64"
+   is "a" "a" $ Just "a == a"
+   isnt "a" "a" $ Just "a == a"
+   like "abc" "a" Nothing
+   unlike "abc" "a" Nothing
+   skip 3 $ Just "rope"
    
 
 
 
 {-
-
-# Used to call _cleanup on shell exit
-trap _exit EXIT
-
 
 # This is the workhorse method that actually
 # prints the tests result.
@@ -231,103 +334,6 @@ ok(){
 }
 
 
-_matches(){
-	local result=${1:?}
-	local pattern=${2:?}
-
-	if [[ -z "$result" || -z "$pattern" ]] ; then
-		return 1
-	elif [[ "$result" =~ "$pattern" ]] ; then
-		return 0
-	else 
-		return 1
-	fi
-}
-
-
-_is_diag(){
-	local result=${1:?}
-	local expected=${2:?}
-
-	diag "         got: '$result'" 
-	diag "    expected: '$expected'"
-}
-
-
-is(){
-	local result=${1:?}
-	local expected=${2:?}
-	local name=${3:-''}
-
-	_equals "$result" "$expected"
-	(( $? == 0 ))
-	ok $? "$name"
-	local r=$?
-	(( r != 0 )) && _is_diag "$result" "$expected"
-	return $r 
-}
-
-
-isnt(){
-	local result=${1:?}
-	local expected=${2:?}
-	local name=${3:-''}
-
-	_equals "$result" "$expected"
-	(( $? != 0 ))
-	ok $? "$name"
-	local r=$?
-	(( r != 0 )) && _is_diag "$result" "$expected"
-	return $r 
-}
-
-
-like(){
-	local result=${1:?}
-	local pattern=${2:?}
-	local name=${3:-''}
-
-	_matches "$result" "$pattern"
-	(( $? == 0 ))
-	ok $? "$name"
-	local r=$?
-	(( r != 0 )) && diag "    '$result' doesn't match '$pattern'"
-	return $r
-}
-
-
-unlike(){
-	local result=${1:?}
-	local pattern=${2:?}
-	local name=${3:-''}
-
-	_matches "$result" "$pattern"
-	(( $? != 0 ))
-	ok $? "$name"
-	local r=$?
-	(( r != 0 )) && diag "    '$result' matches '$pattern'"
-	return $r
-}
-
-
-skip(){
-	local condition=${1:?}
-	local reason=${2:-''}
-	local n=${3:-1}
-
-	if (( condition == 0 )) ; then
-		local i=
-		for (( i=0 ; i<$n ; i++ )) ; do
-			_executed_tests=$(( _executed_tests + 1 ))
-			echo "ok $_executed_tests # skip: $reason" 
-		done
-		return 0
-	else
-		return
-	fi
-}
-
-
 _cleanup(){
 	local rc=0
 
@@ -364,19 +370,6 @@ _cleanup(){
 	fi
 
 	return $rc
-}
-
-
-_exit_status(){
-	if (( _no_plan != 0 || _plan_set == 0 )) ; then
-		return $_failed_tests
-	fi
-
-	if (( _expected_tests < _executed_tests )) ; then
-		return $(( _executed_tests - _expected_tests  ))
-	fi
-
-	return $(( _failed_tests + ( _expected_tests - _executed_tests )))
 }
 
 
